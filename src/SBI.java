1a647a917f168d9921dfc32011b8a67bfe9dffa8
Shipra Sharma <shipra sharma>
2022-10-02 19:11:13 +0530

    Initial commit

---
+++ src/SBI.java
@@ -0,0 +1,12 @@
+public class SBI extends Bank {
+        String branchId;
+        public SBI(String city, float roi, String branchId) {
+            super(city, roi);
+            this.branchId = branchId;
+        }
+        public void getDetails() {
+            super.getDetails();
+            System.out.println("BranchId " + branchId);
+        }
+    }
+
